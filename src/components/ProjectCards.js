625dcb50385b8812b7b8728fba82286ff3ec08a1
Martin Yankov <lutheriscomming@gmail.com>
2020-11-12 15:08:55 +0200

    Linting

--- src/components/ProjectCards.js
+++ src/components/ProjectCards.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react"
 import Img from "gatsby-image"
 
 import { StaticQuery, graphql } from "gatsby"
@@ -6,63 +6,68 @@ import { StaticQuery, graphql } from "gatsby"
 export default () => (
   <StaticQuery
     query={graphql`
-    {
-      projects: allMarkdownRemark(
-        filter: { fileAbsolutePath: { regex: "/projects/"}}
+      {
+        projects: allMarkdownRemark(
+          filter: { fileAbsolutePath: { regex: "/projects/" } }
         ) {
-        edges {
-          node {
-            frontmatter {
-              type
-              title
-              description
-              tools
-              image {
-                childImageSharp{
+          edges {
+            node {
+              frontmatter {
+                type
+                title
+                description
+                tools
+                image {
+                  childImageSharp {
                     sizes(maxWidth: 590) {
-                        ...GatsbyImageSharpSizes
+                      ...GatsbyImageSharpSizes
                     }
+                  }
                 }
-            }
+              }
             }
           }
         }
       }
-    }
     `}
-    render={data => (
-      <header>
-
+    render={data => {
+      console.log(data.projects)
+      return (
+        <header>
           <div className="line-numbers">
-            <p>1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34</p>
+            <p>
+              1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25
+              26 27 28 29 30 31 32 33 34
+            </p>
           </div>
 
-        <div className="divider">
-        </div>
-       
-        {data.projects.edges.map(({ node }) => (
-        <div className="project-container">
+          <div className="divider"></div>
 
-          <div className="project-leftside">
+          {data.projects.edges.map(({ node }) => (
+            <div className="project-container">
+              <div className="project-leftside">
+                <div className="project-text">
+                  <p className="project-type">{node.frontmatter.type}</p>
+                  <p className="project-title">{node.frontmatter.title}</p>
+                  <p className="project-excerpt">
+                    {node.frontmatter.description}
+                  </p>
+                </div>
+                <div className="project-tools">
+                  <p>{node.frontmatter.tools}</p>
+                </div>
+              </div>
 
-            <div className="project-text">  
-              <p className="project-type">{node.frontmatter.type}</p>
-              <p className="project-title">{node.frontmatter.title}</p>
-              <p className="project-excerpt">{node.frontmatter.description}</p>
+              <div className="project-image-container">
+                <Img
+                  className="project-image"
+                  sizes={node.frontmatter.image.childImageSharp.sizes}
+                />
+              </div>
             </div>
-            <div className="project-tools">
-              <p>{node.frontmatter.tools}</p>
-            </div>
-
-          </div>
-
-          <div className="project-image-container">
-            <Img className="project-image" sizes={node.frontmatter.image.childImageSharp.sizes} /> 
-          </div>
-
-        </div>
-      ))}
-      </header>
-    )}
+          ))}
+        </header>
+      )
+    }}
   />
 )
